d9e45b33095caeb53d6703188592f1034f609959
Danilon6 <danilonebulosi@gmail.com>
2024-07-15 11:26:14 +0200

    home changed

--- Phronesis/src/app/pages/home/home.component.ts
+++ Phronesis/src/app/pages/home/home.component.ts
@@ -1,9 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+import { ILoginData } from '../../models/i-login-data';
+import { NotificationService } from '../../services/notification.service';
+import { ErrorHandlingServiceService } from '../../services/error-handling-service.service';
+import { IUser } from '../../models/i-user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  loginUser: ILoginData = {
+    username: 'danilo8',
+    password: 'password'
+  };
+  rememberMe: boolean = false;
+  errorMessage: string = '';
+  showPassword:boolean = false
+
+  constructor(
+    private authSvc: AuthService,
+    private router: Router,
+    private notificationSvc: NotificationService,
+    private errorHandlingSvc: ErrorHandlingServiceService
+  ) { }
+
+  login() {
+    this.authSvc.login(this.loginUser, this.rememberMe).subscribe(() => {
+      this.router.navigate(['/feed']);
+    });
+  }
+
+  newUser: Partial<IUser> = {};
+  file: File | undefined;
+  profileImageUrl: string | ArrayBuffer | null = "https://res.cloudinary.com/ddcghtsnn/image/upload/v1719225290/aavszufvpxsxdrqiyn8s.png";
+
+  register(registerForm: NgForm) {
+    if (registerForm.invalid) {
+      this.notificationSvc.notifyValidationErrors(registerForm);
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('newUser', new Blob([JSON.stringify(this.newUser)], { type: 'application/json' }));
+    if (this.file) {
+      formData.append('profilePictureFile', this.file);
+    }
+
+    this.authSvc.register(formData).subscribe({
+      next: () => {
+        this.router.navigate(['/auth/login']);
+        this.notificationSvc.notify('Registrazione avvenuta con successo', 'success');
+      },
+      error: (error) => this.errorHandlingSvc.handleError(error)
+    });
+  }
+
+  onFileChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      this.file = file;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.profileImageUrl = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  toggleShowPassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  @ViewChild('container') container!: ElementRef;
+
+  signIn() {
+    this.container.nativeElement.classList.remove('right-panel-active');
+  }
+
+  signUp() {
+    this.container.nativeElement.classList.add('right-panel-active');
+  }
 }
--- Phronesis/src/app/pages/home/home.module.ts
+++ Phronesis/src/app/pages/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FooterComponent } from '../../mainComponents/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FooterComponent } from '../../mainComponents/footer/footer.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
